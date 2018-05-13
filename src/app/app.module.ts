572187d893e07150ba32d7dd34aa3977640af749
Lurance <353650263@qq.com>
2018-05-13 11:40:48 +0800

    做了个首页

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ArticleListComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/model/article.ts
@@ -0,0 +1,9 @@
+export class Article {
+  id: number;
+  verboseId: string;
+  title: string;
+  content: string;
+  createdAt: number;
+  tag: string;
+  like: number;
+}
